6aa0f2b2bbae4e7580270bb0e68735b814c6eee1
Drumor <ludovic.taffin@uclouvain.be>
2018-06-27 16:21:44 +0200

    add bootstrap and basic templating and SSO

--- app/core/routes.py
+++ app/core/routes.py
@@ -1,6 +1,41 @@
 from app.core import main
+from flask import redirect, session, render_template
+
+
+def get_user_session_info(key):
+    return session['user'].get(
+        key,
+        'Key `{0}` not found in user session info'.format(key)
+    )
+
+
+def get_user_details(fields):
+    defs = [
+        '<dt>{0}</dt><dd>{1}</dd>'.format(f, get_user_session_info(f))
+        for f in fields
+    ]
+    return '<dl>{0}</dl>'.format(''.join(defs))
 
 
 @main.route('/')
 def index():
-    return 'hello world!!!'
+
+    if 'user' in session:
+        details = get_user_details([
+            'eppn',
+            'uid',
+            'givenName',
+            'mail',
+            'sn',
+            'affiliation',
+            'displayName',
+            'title'
+        ])
+    else:
+        details = ''
+    return render_template('index.html',details=details)
+
+@main.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect('/')
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from app import create_app
 
 if __name__=='__main__':
-
-    flask_app = create_app('dev')
+    env = 'dev'
+    flask_app = create_app(env)
+    flask_app.env=env
+    flask_app.debug=True
     flask_app.run()
