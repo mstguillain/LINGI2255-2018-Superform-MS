2eeda2646ce05c7af9a0e75229b38be24b076991
Drumor <ludovic.taffin@uclouvain.be>
2018-07-13 17:40:11 +0200

    adding skeleton "new channel" feature

--- superform/superform/__init__.py
+++ superform/superform/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, session, request, redirect, url_for
 
-from superform.models import User,Channel, db as models_db
+from superform.models import User, Channel, db as models_db
 from superform.authentication import authentication_page, db as authentication_db
 from superform.authorizations import authorizations_page, db as authorizations_db
 from superform.utils import login_required
-
 
 app = Flask(__name__)
 app.config.from_json("config.json")
@@ -18,6 +17,7 @@ authentication_db.init_app(app)
 models_db.init_app(app)
 authorizations_db.init_app(app)
 
+
 @app.route('/')
 def index():
     user = User.query.get(session.get("user_id", "")) if session.get("logged_in", False) else None
@@ -26,8 +26,8 @@ def index():
 
 @app.route('/records')
 def records():
-    records = [[1,"sub1","body1","FB INGI"],[2,"sub2","body2","Portail ICTEAM"]]
-    return render_template('records.html', records = records)
+    records = [[1, "sub1", "body1", "FB INGI"], [2, "sub2", "body2", "Portail ICTEAM"]]
+    return render_template('records.html', records=records)
 
 
 @app.route('/new')
@@ -38,20 +38,18 @@ def new_post():
     else:
         return render_template('done.html')
 
-@app.route('/new_channel', methods=['GET','POST'])
-@login_required
+
+@app.route('/new_channel', methods=['GET', 'POST'])
+@login_required()
 def new_channel():
     if request.method == "GET":
-        return render_template('new_channel.html',pluginparams={})
+        return render_template('new_channel.html', pluginparams={})
     elif request.method == "POST":
         channelname = request.form.get('chanName')
-        c = Channel(name=channelname,module="mail",config={})
+        c = Channel(name=channelname, module="mail", config={})
         models_db.session.add(c)
         models_db.commit()
         return redirect(url_for('index'))
-
-
-
 
 
 @app.errorhandler(403)
