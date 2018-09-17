f1046101c067efaa828b87992604e53115b01ac5
Drumor <ludovic.taffin@uclouvain.be>
2018-09-17 11:28:01 +0200

    cleaning code

--- superform/superform/channels.py
+++ superform/superform/channels.py
@@ -33,10 +33,11 @@ def channel_list():
             db.session.commit()
 
     channels = Channel.query.all()
-    return render_template("channels.html", channels=channels, modules=get_modules_names(current_app.config["PLUGINS"].keys()))
+    return render_template("channels.html", channels=channels,
+                           modules=get_modules_names(current_app.config["PLUGINS"].keys()))
 
 
-@channels_page.route("/configure/<int:id>", methods=['GET','POST'])
+@channels_page.route("/configure/<int:id>", methods=['GET', 'POST'])
 @login_required(admin_required=True)
 def configure_channel(id):
     c = Channel.query.get(id)
@@ -45,18 +46,18 @@ def configure_channel(id):
     config_fields = clas.CONFIG_FIELDS
 
     if request.method == 'GET':
-        if(c.config is not ""):
+        if (c.config is not ""):
             d = ast.literal_eval(c.config)
             setattr(c, "config_dict", d)
-        return render_template("channel_configure.html",channel = c, config_fields = config_fields)
-    str_conf="{"
-    cfield =0
+        return render_template("channel_configure.html", channel=c, config_fields=config_fields)
+    str_conf = "{"
+    cfield = 0
     for field in config_fields:
         if cfield > 0:
-            str_conf = str_conf +","
-        str_conf = str_conf+"\""+field+"\" : \""+request.form.get(field)+"\""
-        cfield = cfield +1
-    str_conf = str_conf+"}"
-    c.config=str_conf
+            str_conf += ","
+        str_conf += "\"" + field + "\" : \"" + request.form.get(field) + "\""
+        cfield += 1
+    str_conf += "}"
+    c.config = str_conf
     db.session.commit()
     return redirect(url_for('channels.channel_list'))
