07bda2c6bc937286d51567845bd017fb2d1c0c4c
mcomans <marc-henry.comans@student.uclouvain.be>
2018-10-17 22:51:26 +0200

    new plugin for LinkedIn

---
+++ superform/superform/plugins/LinkedIn.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from smtplib import SMTPException
+from flask import current_app
+import json
+
+FIELDS_UNAVAILABLE = ['Title','Description']
+
+CONFIG_FIELDS = ["sender","receiver"]
+
+def run(publishing,channel_config):
+    json_data = json.loads(channel_config)
+    sender = json_data['Account']
+    receivers = json_data['Login']
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = receivers
+    msg['Subject'] = publishing.title
+
+    body = publishing.description
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        smtpObj = smtplib.SMTP(current_app.config["SMTP_HOST"],current_app.config["SMTP_PORT"])
+        if current_app.config["SMTP_STARTTLS"]:
+            smtpObj.starttls()
+        text = msg.as_string()
+        smtpObj.sendmail(sender, receivers, text)
+        smtpObj.quit()
+    except SMTPException as e:
+        #TODO should add log here
+        print(e)
