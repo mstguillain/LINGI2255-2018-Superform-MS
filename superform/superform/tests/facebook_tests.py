f433389eeda98a82482f4ed226819ff43aea18e1
Roussieau <julian.roussieau@student.uclouvain.be>
2018-10-18 17:58:44 +0200

    update to unittest 3.6

--- superform/superform/tests/facebook_tests.py
+++ superform/superform/tests/facebook_tests.py
@@ -5,7 +5,7 @@ import unittest
 
 class FacebookTest(unittest.TestCase):
     def test_publish(self):
-        msg = 'Ceci est une publication !'
+        msg = "Test d'une publication"
         cfg = {
             "page_id"      : "285844238930581",  # Step 1
             "access_token" : "EAAHcEGT1yyEBAOrJr07ZC71bLrPryUUY2luLaLgC7jrycFDLbJ857W3RVLvIru0Sh8WyR4Udr8YA2OCtDDEWEtJrNoBKYMTZAjIMwZAW9ZAtaLvh76cX7xCZBrBFEq3w2B7NdjCPI2z7tAiyGzZCaLsFx4mPyFoSQZCkYCnbaXOpAZDZD"   # Step 3
@@ -14,7 +14,7 @@ class FacebookTest(unittest.TestCase):
         api = get_api(cfg)
         publication_id = publish(msg)
         pub = api.get_object(id=publication_id, fields='message')
-        self.assertEquals(msg, pub['message'])
+        self.assertEqual(msg, pub['message'])
         delete(publication_id)
 
 if __name__ == '__main__':
