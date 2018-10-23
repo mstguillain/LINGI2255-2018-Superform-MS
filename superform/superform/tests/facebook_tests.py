153aa09706ba9006bae366e03d37c2835141b96a
Roussieau <julian.roussieau@student.uclouvain.be>
2018-10-23 23:48:41 +0200

    Plus besoin de hardcoder le token dans le test

--- superform/superform/tests/facebook_tests.py
+++ superform/superform/tests/facebook_tests.py
@@ -6,12 +6,9 @@ import unittest
 class FacebookTest(unittest.TestCase):
     def test_publish(self):
         msg = "Test d'une publication !!!"
-        cfg = {
-            "page_id"      : "285844238930581",  # Step 1
-            "access_token" : "EAAHcEGT1yyEBAOSo50WUiM0563zVZCxnZCdd2XVJRAll0wIFztF4m1pPfw5hqlDwbvqZBCMNlnqzlTZC2RSkzDSTzwJZBhJuyZBcyAIgyIiSBqEZBV4GbyVhKJLWlCuZByRyn6AxgTFtGMeJwEQqvBCjZCHGZBfb5qBy2H27x2z6dMygDbJiltxSZCRLMSYAWE4QYrUwLgQIVxUHgZDZD"   # Step 3
-        }
 
-        api = get_api(cfg)
+
+        api = get_api(CFG)
         publication_id = publish(msg)
         pub = api.get_object(id=publication_id, fields='message')
         self.assertEqual(msg, pub['message'])
