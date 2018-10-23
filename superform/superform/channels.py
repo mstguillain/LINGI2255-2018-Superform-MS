39d412cc7249a06dd403ff676425cb390357965e
Yannick Schlesser <yannick.schlesser@student.uclouvain.be>
2018-10-23 10:43:14 +0200

    push

--- superform/superform/channels.py
+++ superform/superform/channels.py
@@ -1,22 +1,27 @@
-from flask import Blueprint, current_app, url_for, request, make_response, redirect, session, render_template
+from flask import Blueprint, current_app, url_for, request, make_response, \
+    redirect, session, render_template
 
-from superform.utils import login_required, get_instance_from_module_path, get_modules_names, get_module_full_name
+from superform.utils import login_required, get_instance_from_module_path, \
+    get_modules_names, get_module_full_name
 from superform.models import db, Channel
 import ast
 
 channels_page = Blueprint('channels', __name__)
 
 
-@channels_page.route("/channels", methods=['GET', 'POST'])
-@login_required(admin_required=True)
+@channels_page.route("/channels", methods = ['GET', 'POST'])
+@login_required(admin_required = True)
 def channel_list():
     if request.method == "POST":
         action = request.form.get('@action', '')
         if action == "new":
             name = request.form.get('name')
             module = request.form.get('module')
-            if module in get_modules_names(current_app.config["PLUGINS"].keys()):
-                channel = Channel(name=name, module=get_module_full_name(module), config="{}")
+            if module in get_modules_names(
+                    current_app.config["PLUGINS"].keys()):
+                channel = Channel(name = name,
+                                  module = get_module_full_name(module),
+                                  config = "{}")
                 db.session.add(channel)
                 db.session.commit()
         elif action == "delete":
@@ -33,12 +38,13 @@ def channel_list():
             db.session.commit()
 
     channels = Channel.query.all()
-    return render_template("channels.html", channels=channels,
-                           modules=get_modules_names(current_app.config["PLUGINS"].keys()))
+    return render_template("channels.html", channels = channels,
+                           modules = get_modules_names(
+                               current_app.config["PLUGINS"].keys()))
 
 
-@channels_page.route("/configure/<int:id>", methods=['GET', 'POST'])
-@login_required(admin_required=True)
+@channels_page.route("/configure/<int:id>", methods = ['GET', 'POST'])
+@login_required(admin_required = True)
 def configure_channel(id):
     c = Channel.query.get(id)
     m = c.module
@@ -49,7 +55,8 @@ def configure_channel(id):
         if (c.config is not ""):
             d = ast.literal_eval(c.config)
             setattr(c, "config_dict", d)
-        return render_template("channel_configure.html", channel=c, config_fields=config_fields)
+        return render_template("channel_configure.html", channel = c,
+                               config_fields = config_fields)
     str_conf = "{"
     cfield = 0
     for field in config_fields:
--- superform/superform/plugins/LinkedIn.py
+++ superform/superform/plugins/LinkedIn.py
@@ -5,8 +5,7 @@ from smtplib import SMTPException
 from flask import current_app
 import json
 
-
-# from linkedin import linkedin
+#from linkedin import linkedin
 
 #######################
 # Copied from mail.py #
@@ -14,42 +13,57 @@ import json
 
 FIELDS_UNAVAILABLE = ['Title', 'Description']
 
-CONFIG_FIELDS = ["account", "login"]
+CONFIG_FIELDS = ["Account", "Login", "Link to authentication"]
 
 
 # TODO to change according to the api
-def run(publishing, channel_config):
-    json_data = json.loads(channel_config)
-    account = json_data['account']
-    login = json_data['login']
-    msg = MIMEMultipart()
-    msg['From'] = account
-    msg['To'] = login
-    msg['Subject'] = publishing.title
+#def run(publishing, channel_config):
+#    json_data = json.loads(channel_config)
+#    account = json_data['account']
+#    login = json_data['login']
+#    msg = MIMEMultipart()
+#    msg['From'] = account
+#    msg['To'] = login
+#    msg['Subject'] = publishing.title
 
-    body = publishing.description
-    msg.attach(MIMEText(body, 'plain'))
+#    body = publishing.description
+#    msg.attach(MIMEText(body, 'plain'))
 
-    try:
-        smtpObj = smtplib.SMTP(current_app.config["SMTP_HOST"],
-                               current_app.config["SMTP_PORT"])
-        if current_app.config["SMTP_STARTTLS"]:
-            smtpObj.starttls()
-        text = msg.as_string()
-        smtpObj.sendmail(account, login, text)
-        smtpObj.quit()
-    except SMTPException as e:
-        # TODO should add log here
-        print(e)
+#    try:
+#        smtpObj = smtplib.SMTP(current_app.config["SMTP_HOST"],
+#                               current_app.config["SMTP_PORT"])
+#        if current_app.config["SMTP_STARTTLS"]:
+#            smtpObj.starttls()
+#        text = msg.as_string()
+#        smtpObj.sendmail(account, login, text)
+#        smtpObj.quit()
+#    except SMTPException as e:
+#        # TODO should add log here
+#        print(e)
 
 
 ########################################################
-# Taken from https://pypi.org/project/python-linkedin/ #
+# Taken from https://pypi.org/project/python3-linkedin/ #
 ########################################################
 
-#def login():
-#    CONSUMER_KEY = "77p0caweo4t3t9"
-#    CONSUMER_SECRET = "uQVYTN3pDewuOb7d"
+def login():
+    client_ID = '77p0caweo4t3t9'
+    client_secret = 'uQVYTN3pDewuOb7d'
+    RETURN_URL = 'http://localhost:5000/'  # TODO the configure url, to be changed on the LinkeIn Application service
+    state = '12345'
+    redirect1 = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=77p0caweo4t3t9&redirect_uri=http://localhost:5000/%2Fauth%2Flinkedin&state=987654321&'
+    redirect1bis = "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=", client_ID, "&redirect_uri=", RETURN_URL, "%2Fauth%2Flinkedin&state=", state, "&"
+    # TODO user redirected to redirect1bis
+    # TODO testing the returned state value
+
+    authentication = linkedin.LinkedInAuthentication(client_ID, client_secret,
+                                                     RETURN_URL,
+                                                     linkedin.PERMISSIONS.enums.values())
+    print(authentication.authorization_url)
+    application = linkedin.LinkedInApplication(authentication)
+
+#    CONSUMER_KEY = ""
+#    CONSUMER_SECRET = ""
 #    USER_SECRET="" ?
 #    USER_TOKEN="" ?
 #    RETURN_URL = 'http://localhost:5000'
@@ -61,7 +75,7 @@ def run(publishing, channel_config):
 
 # Instantiate the developer authentication class
 
-#authentication = linkedin.LinkedInDeveloperAuthentication(
+# authentication = linkedin.LinkedInDeveloperAuthentication(
 #                    CONSUMER_KEY,
 #                    CONSUMER_SECRET,
 #                    USER_TOKEN,
