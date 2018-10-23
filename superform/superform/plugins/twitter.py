c5b5ad5b0c39bc2d2a3fa7095285265c7d46a38d
Nicolas Golenvaux <nicolas.golenvaux@student.uclouvain.be>
2018-10-23 15:34:41 +0200

    add the support of the publications longer than 280 characters add the support of link

--- superform/superform/plugins/twitter.py
+++ superform/superform/plugins/twitter.py
@@ -1,5 +1,8 @@
 import json
 import twitter
+import datetime
+
+import datetime
 
 FIELDS_UNAVAILABLE = ['Title']
 CONFIG_FIELDS = ["consumer_key","consumer_secret","access_token_key","access_token_secret"]
@@ -15,6 +18,11 @@ def is_valid_tweet(tweet):
     else:
         return True
 
+def is_time_to_publish(publishing) :
+    date=publishing.date_from
+    return  date <= datetime.datetime.now()
+
+
 def run(publishing,channel_config):
     json_data = json.loads(channel_config)
     api = twitter.Api(consumer_key = json_data['consumer_key'],
@@ -27,4 +35,4 @@ def run(publishing,channel_config):
     if is_valid_tweet(tweet): # For the moment, we avoid the tweet if it's not valid
         api.PostUpdate(status=publishing.description)
     else :
-        api.PostUpdates(status  = publishing.description, continuation="[...]")
+        api.PostUpdates(status = publishing.description, continuation="[...]")
