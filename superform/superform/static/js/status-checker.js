5b9e86ba1eae644a8a598c18a0d679ae1caa04f7
nsias <nicolas.sias.01@gmail.com>
2018-11-08 16:56:32 +0100

    [Status-checker] Better generalisation + recreate render for tweets

--- superform/superform/static/js/status-checker.js
+++ superform/superform/static/js/status-checker.js
@@ -1,7 +1,35 @@
 /*
 * CUSTOM BY GROUP 10
 */
-PLUGINS_TO_CHECK = ["mail","wiki", "twitter"];
+LIMIT_CHAR_TWEET = 280;
+LIMIT_NB_TWEET = 10;
+
+PLUGINS_TO_CHECK = ["twitter"];
+
+statusListener = {
+    twitter :
+    [
+        {
+            func : "LIMIT_CONTENT",
+            text : "If you hit publish, this post will be split in multiple tweets",
+            canPublish : true,
+            value : LIMIT_CHAR_TWEET
+        },
+        {
+            func : "NO_TITLE",
+            text : "The title will be not displayed",
+            canPublish : true
+        },
+        {
+            func : "RENDER_FOR_TWEET",
+            canPublish : true,
+            value : LIMIT_CHAR_TWEET
+        }
+    ]
+};
+
+
+
 
 statusChecker = {
     _lengthTitle : 0,
@@ -54,31 +82,13 @@ statusChecker.checking(function(){
         removeStatusBox(item);
     });
 
-    this._pluginChecked.forEach(function(item, index){
-        addStatusBox(item);
-        window[item]();
+    this._pluginChecked.forEach(function(plugin, index){
+        addStatusBox(plugin);
+        statusListener[plugin].forEach(function (item, index2) {
+            window[item["func"]]("twitter", item["text"], item["value"], item["canPublish"]);
+        });
     });
 });
-
-
-function twitter() {
-    if (statusChecker.lengthContent > 280) {
-        addStatus("twitter","If you hit publish, this post will be split in multiple tweets","tweet-too-long");
-    }
-    else {
-        removeStatus("tweet-too-long");
-    }
-
-}
-
-function mail() {
-    addStatus("mail", "TODO", "thisid");
-}
-
-function wiki() {
-    addStatus("wiki", "TODO", "thisid");
-}
-
 
 
 /*
@@ -109,8 +119,12 @@ $( "#titlepost" ).on('input', function() {
 });
 
 // Content
-$( "#descriptionpost" ).on('input', function() { //TO DO REFACTOR FOR TWITTER
-    statusChecker.lengthContent = $(this).val().length;
+$( "#descriptionpost" ).on('input', function() {
+    if (isInArray(statusChecker.pluginChecked, "twitter"))
+        statusChecker.lengthContent = $(this).val().length + tweets.toString().length;
+    else
+        statusChecker.lengthContent = $(this).val().length;
+    console.log((isInArray(statusChecker.pluginChecked, "twitter")));
 });
 
 // Link URL
@@ -121,12 +135,12 @@ $( "#linkurlpost" ).on('input', function() {
 
 
 /*
-* Utility functions
+* Utility functions to render status
 */
 
 function addStatusBox(name) {
     var title = name.substring(0,1).toUpperCase() + name.substring(1,name.length);
-    if($("#status-"+name).length == 0)  { // If it doesn't exist, create it
+    if($("#status-"+name).length == 0)   { // If it doesn't exist, create it
         $("#status-content").append("<ul class=\"list-group\" id=\"status-"+name+"\"><li class=\"list-group-item active\">"+title+"</li> </ul>");
     }
 }
@@ -135,17 +149,74 @@ function removeStatusBox(name) {
     $("#status-"+name).remove();
 }
 
-function addStatus(plugin, text, id) {
+function addStatusText(plugin, text, id) {
     if ($("#"+id).length == 0) { // If it doesn't exist, create it
          $("#status-"+plugin).append("<li class=\"list-group-item\" id=\""+plugin+"-"+id+"\">"+text+"</li>");
     }
 }
 
-function removeStatus(plugin, id) {
+function removeStatusText(plugin, id) {
     if ($("#"+id).length != 0) { // If it doesn't exist, create it
          $("#status-"+plugin+"-"+id).remove();
     }
 }
+
+/*
+*   Listeners function
+*/
+
+function LIMIT_CONTENT(plugin, text, value, canPublish) {
+    if (statusChecker.lengthContent  > value) {
+        var id = plugin + "content-too-long";
+        addStatusText(plugin, text, id);
+    }
+    console.log(statusChecker.lengthContent + NB_TWEET * 280)
+}
+
+function LIMIT_TITLE(plugin, text, value, canPublish) {
+    if (statusChecker.lengthTitle > value) {
+        var id = plugin + "title-too-long";
+        addStatusText(plugin, text, id);
+    }
+}
+
+function LIMIT_URL(plugin, text, value, canPublish) {
+    if (statusChecker.lengthURL > value) {
+        var id = plugin + "url-too-long";
+        addStatusText(plugin, text, id);
+    }
+}
+
+function NO_CONTENT(plugin, text, value, canPublish) {
+    if (statusChecker.lengthContent == 0)  {
+        var id = plugin +"-no-content";
+        addStatusText(plugin,text,id);
+    }
+}
+function NO_TITLE(plugin, text, value, canPublish) {
+    if (statusChecker.lengthTitle == 0)  {
+        var id = plugin +"-no-title";
+        addStatusText(plugin,text,id);
+    }
+}
+function NO_URL(plugin, text, value, canPublish) {
+    if (statusChecker.lengthTitle == 0)  {
+        var id = plugin +"-no-url";
+        addStatusText(plugin,text,id);
+    }
+}
+
+function RENDER_FOR_TWEET(plugin, text, value, canPublish) {
+    if (statusChecker.lengthContent > value) {
+        renderTweet();
+    }
+}
+
+/*
+function addStatus(func, text, block, val = 0) {
+    window[func](text, block, val);
+}
+*/
 
 // Utily function for array
 function isInArray(tb, value) {
@@ -158,10 +229,46 @@ function isInArray(tb, value) {
 }
 
 function removeFromArray(tb, value) {
-    tb2 = new Array();
+    var tb2 = new Array();
     tb.forEach(function(item, index) {
         if (item != value)
             tb2.push(item);
     });
     return tb2;
 }
+
+/*
+*  Render tweet part
+*/
+/*
+tweets = new Array();
+NB_TWEET = 0;
+
+function createInputTextForTweet(text, nbTweet) {
+
+    var inputToAdd = '<div id=tweet-"'+nbTweet+'>';
+    inputToAdd += '<textarea class="form-control" rows="5" id="descriptionpost" name="descriptionpost" maxlength="'+LIMIT_CHAR_TWEET+'">'
+    inputToAdd += text
+    inputToAdd += '</textarea><br/><br/></div>';
+    if (nbTweet == NB_TWEET - 1)
+        $(inputToAdd).insertBefore('#descriptionpost');
+    else
+        $(inputToAdd).insertBefore('#tweet-'+nbTweet - 1);
+}
+
+function renderTweet() {
+    //Create array of tweets
+    var sizeArray = 0;
+    for (var i = 0, charsLength = $("#descriptionpost").val().length; i < charsLength; i += 280) {
+        tweets.push($("#descriptionpost").val().substring(i, i + 280));
+        NB_TWEET = NB_TWEET + 1;
+    }
+    //Create each input based on this
+    tweets.forEach(function(item, index) {
+        createInputTextForTweet(text, nbTweet);
+    });
+
+}
+
+
+*/
