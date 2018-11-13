40e5dacf63004f91aea54501506d66f72cab05dc
Jean <tip.hon@hotmail.fr>
2018-11-13 22:19:37 +0100

    FeedParser library

--- superform/setup.py
+++ superform/setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[
         'flask',
         'python3-saml',
-        'sqlalchemy',
+        'sqlalchemy','feedparser',
         'flask-sqlalchemy', 'python3-linkedin', 'facebook-sdk', 'python-twitter'
     ],
 )
