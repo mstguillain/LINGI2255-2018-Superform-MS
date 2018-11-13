6f4483c7eb0ccbbd207ce6ece69af7ec4dc45c31
mcomans <marc-henry.comans@student.uclouvain.be>
2018-11-14 00:22:38 +0100

    publication RSS ok

--- superform/superform/plugins/rss.py
+++ superform/superform/plugins/rss.py
@@ -1,15 +1,104 @@
-import feedparser
 from flask import current_app
 import json
+import rfeed
+import datetime
+import os
+import feedparser
 
-FIELDS_UNAVAILABLE = ['Title', 'Description']
+FIELDS_UNAVAILABLE = ['Name of the Feed', 'Description']
 
-CONFIG_FIELDS = ["sender", "receiver"]
+CONFIG_FIELDS = ["rname","rdescription"]
 
+RSS_DIR = "localhost:5000/rss/"
+
+def newFeed(rname,rdescription):
+    """
+    :param rname: name for the RSS feed
+    :param rdescription: description of the RSS feed
+    :return: Will return a new (empty publication) feed with "localhost:5000/rss/NAME_OF_FEED.xml" as link
+    """
+    print("ici")
+    temp = rname.split(" ")
+    nameOfFeed = "_".join(temp)
+    feedLink = RSS_DIR+nameOfFeed+".xml"
+    feed = rfeed.Feed(
+        title=rname,
+        link=feedLink,
+        description=rdescription,
+        lastBuildDate=datetime.datetime.now(),
+        docs=None,
+        items=[])
+    print("link: ",feedLink)
+    return feed
+
+def import_items(xml_path):
+    """
+    :param xml_path: the path to an existing xml file
+    :return: return the list of all previous publication (items) on the feed.
+    Since when we publish, we overwrite the xml file, we need the history of the publication to re-write them aswell.
+    """
+    items = list()
+    d = feedparser.parse(xml_path)
+
+    for post in d.entries:
+        title = None
+        link = None
+        body = None
+        date = None
+        if 'title' in post:
+            title = post.title
+            # print('title ok')
+        if 'link' in post:
+            link = post.link
+            # print('link ok')
+        if 'description' in post:
+            body = post.description
+            # print('description ok')
+        if 'published' in post:
+            date= post.published
+            # print('date ok')
+
+        item = rfeed.Item(
+            title=title,
+            link=link,
+            description=body,
+            pubDate=datetime.datetime.strptime(date, "%a, %d %b %Y %X GMT"))
+        items.append(item)
+    return items
 
 def run(publishing, channel_config):
+
     json_data = json.loads(channel_config)
-    sender = json_data['sender']
-    receivers = json_data['receiver']
-    title = publishing.title
-    body = publishing.description
+    rname = json_data['rname']
+    rdescription = json_data['rdescription']
+    item_title = publishing.title
+    item_body = publishing.description
+    item_link = publishing.link_url
+    item_from = publishing.date_from
+    item_until = publishing.date_until
+    item_img = publishing.image_url
+
+    item = rfeed.Item(
+        title=item_title,
+        link=item_link,
+        description=item_body,
+        pubDate=item_from)
+
+
+    localPath = os.path.dirname(__file__)+"/rss/feed_"+str(publishing.channel_id)+".xml"
+    feed = newFeed(rname, rdescription)
+    feed.items.append(item)
+    if os.path.isfile(localPath): #import older publishing if any
+        print("a file already exist")
+        olderItems = import_items(localPath)
+        print(olderItems)
+        print(len(olderItems))
+        feed.items.extend(olderItems)
+
+
+    print(feed.items)
+    with open(localPath, 'w') as f:
+        a = feed.rss()
+        f.write(a)
+
+
