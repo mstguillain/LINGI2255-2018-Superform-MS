02c4bb1d592dcd3af037c51be676f80fa095ed97
hbaudhuin <heloise.baudhuin@student.uclouvain.be>
2018-11-15 15:15:26 +0100

    [Test ][Wiki] test on run function

--- superform/superform/tests/test_wiki.py
+++ superform/superform/tests/test_wiki.py
@@ -4,7 +4,6 @@ import tempfile
 import datetime
 import string
 import random
-from twitter import twitter_utils
 
 #import pytest
 
@@ -39,6 +38,34 @@ def test_makeText():
     assert tab_of_text[1] == "-----" + "Par " + "Superform" + " Publie le " + date
     assert tab_of_text[2]==  str(pub.description).replace("\n","[[<<]]")
     assert tab_of_text[3]== "-----"+"[["+pub.link_url+"]]"
+
+def test_run():
+    config ={ 'username' : ['superform'], 'password' : ['superform']   }
+    pub = Publishing()
+    pub.date_from = '13.02.02'
+    pub.title = 'test-Title'
+    pub.link_url = 'blablablablablablablajdsfvjdbvjdnfvqebdnbqdfnvsdùnvbmqknkfnbùsfvdf'
+    pub.description = 'descr'
+    pub.image_url = 'imague url'
+    pub.date_until = '14.02.16'
+    pub.state = 1
+    pub.channel_id = 'Wiki'
+    test_correct_wiki_post(pub, config)
+    test_uncorrect_config(pub)
+
+def test_uncorrect_config(publishing) :
+    answer =wiki.run(publishing, 'coucou')
+    assert answer == "error json decoder"
+
+def test_correct_wiki_post(publishing, config) :
+    answer=wiki.run(publishing, config)
+    assert answer.status_code==200
+
+
+
+
+
+
 
 
 
