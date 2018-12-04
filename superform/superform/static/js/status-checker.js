97917fd55bed4c6cc03294e16bf3a24cc066ae5f
nsias <nicolas.sias.01@gmail.com>
2018-12-04 23:36:36 +0100

    [Twitter module] Final (before doc and clean) front-end twitter + implementation for back-end (add to db)

--- superform/superform/static/js/status-checker.js
+++ superform/superform/static/js/status-checker.js
@@ -383,7 +383,8 @@ function unifyTweet() {
 }
 
 $("#publish-button").on('click', function() {
-    jsonTweet = JSON.stringify(tweets);
-    text = '<div class="form-group" hidden><label for="tweets">Tweet</label><br><input type="text" name="tweets" id="tweets" class="form-control" value='+jsonTweet+'></div>';
+    strTweets = JSON.stringify(tweets);
+    text = '<div class="form-group"><label for="tweets">Tweet</label><br><input type="text" name="tweets" id="tweets" class="form-control"></div>';
     $(text).insertBefore(this);
+    $('#tweets').val(strTweets);
 });
