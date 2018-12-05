19cabda403193774cb3eea66dc153c5a33562415
Darkyne <gregory.creupelandt@student.uclouvain.be>
2018-12-05 17:56:53 +0100

    Export to PDF is working from the channel but not from export button on previous posts

--- superform/superform/plugins/pdf.py
+++ superform/superform/plugins/pdf.py
@@ -3,6 +3,8 @@
 @date: December 2018
 """
 from flask import url_for, redirect
+from reportlab.pdfgen import canvas
+import json
 
 FIELDS_UNAVAILABLE = []
 
@@ -11,24 +13,71 @@ CONFIG_FIELDS = []
 
 def run(publishing, channel_config):
     """ Gathers the informations in the config column and launches the
-    posting process """
+    posting process
+    channel_config format = {image : ??, size : "A4"}"""
     # TODO
+    json_data = json.loads(channel_config)
+    title = publishing.title
+    body = publishing.description
+    image = json_data['image'] # HOW WILL THE IMAGE BE STORED IN THE DB ?
+    size = json_data['size']
+    create_pdf(title, body, image, size)
 
 
 def export():
-    """
 
-    :return:
-    """
     print("Here is the export method")
     # TODO
     return redirect(url_for('index'))
 
 
-def create_pdf():
-    """
+def create_pdf(titre, corps, image, size):
+    if(size=="A5"):
+        realSize=(420.94, 595.27)
+    elif(size=="A4"):
+        realSize=(595.27, 841.89)
+    elif(size=="A3"):
+        realSize=(841.27, 1190.54)
 
-    :return:
-    """
-    print("Here is the create_pdf method")
-    # TODO
+    pdf = canvas.Canvas(titre + ".pdf", realSize)
+
+    write_logo(pdf,image, realSize)
+    write_title(pdf, titre, realSize)
+    write_body(pdf,corps,realSize)
+    pdf.save()
+
+
+def write_logo(canvas, image, realSize):
+    leftMarge = realSize[1]*0.4
+    topMarge = realSize[1]*0.85
+    canvas.drawImage(image, leftMarge,topMarge )
+
+
+
+def write_title(canvas, titre, realSize):
+
+    myTitle = canvas.beginText()
+    myTitle.setCharSpace(3)
+    leftMarge = realSize[1]*0.1
+    topMarge = realSize[1]*0.9
+    myTitle.setTextOrigin(leftMarge,topMarge)
+    myTitle.textLine(titre)
+    canvas.drawText(myTitle)
+
+
+def write_body(canvas, body, realSize):
+
+    canvas.setFont("Times-Roman", 10*realSize[1]/800)
+    myText = canvas.beginText()
+    myText.setCharSpace(3)
+    leftMarge = realSize[0]*0.1
+    topMarge = realSize[1]*0.8
+    myText.setTextOrigin(leftMarge,topMarge)
+    words = body.split()
+    n = 9  # number of words per line
+    lignes = [' '.join(words[i:i + n]) for i in range(0, len(words), n)]
+
+    for i in range(0, len(lignes) - 1):
+        myText.textLine(lignes[i])
+    canvas.drawText(myText)
+
