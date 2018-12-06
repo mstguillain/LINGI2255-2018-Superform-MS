ac7f0be60d122ff800e9f4d0adb66e6afe2c990c
Jean <tip.hon@hotmail.fr>
2018-12-06 19:11:47 +0100

    Code beautify
    Signed-off-by: Jean <tip.hon@hotmail.fr>

--- superform/superform/plugins/pdf.py
+++ superform/superform/plugins/pdf.py
@@ -43,11 +43,11 @@ def run(publishing, channel_config):
     import webbrowser
     webbrowser.open_new_tab('file://' + path)
 
-    #data_folder = Path("superform/plugins/pdf")
-    #file_to_delete = data_folder / outputFile
-    #file_to_delete = "DELETEMEPLEASE.txt"
-    #time.sleep(1)
-    #os.remove(file_to_delete)
+    # data_folder = Path("superform/plugins/pdf")
+    # file_to_delete = data_folder / outputFile
+    # file_to_delete = "DELETEMEPLEASE.txt"
+    # time.sleep(1)
+    # os.remove(file_to_delete)
 
 
 def export(post_id, idc):
@@ -67,13 +67,12 @@ def export(post_id, idc):
 
 
 def create_pdf(titre, corps, image, size):
-
     empryString = ""
     fileTitle = empryString.join(e for e in titre if e.isalnum())
-    if(len(fileTitle))==0:
-        fileTitle="DEFAULT"
-    outfilename = fileTitle+".pdf"
-    localPath = os.path.dirname(__file__)+"/pdf/"+outfilename
+    if (len(fileTitle)) == 0:
+        fileTitle = "DEFAULT"
+    outfilename = fileTitle + ".pdf"
+    localPath = os.path.dirname(__file__) + "/pdf/" + outfilename
 
     doc = SimpleDocTemplate(localPath, pagesize=letter,
                             rightMargin=72, leftMargin=72,
@@ -81,18 +80,17 @@ def create_pdf(titre, corps, image, size):
 
     Story = []
 
-    #Adding logo
-    print("image path=",image)
+    # Adding logo
+    print("image path=", image)
     print(os.curdir)
 
-
-    im = Image(image+".png")  # , 2 * inch, 2 * inch)
+    im = Image(image + ".png")  # , 2 * inch, 2 * inch)
     Story.append(im)
     styles = getSampleStyleSheet()
     styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY, leading=15))
     Story.append(Spacer(1, 20))
 
-    #Adding title
+    # Adding title
     p_font = 24
     Story.append(Spacer(1, 30))
     # ptext = '<font name=HELVETICA>'+title+'</font>' % p_font
@@ -101,14 +99,14 @@ def create_pdf(titre, corps, image, size):
     Story.append(Paragraph(rr, styles["Normal"]))
     Story.append(Spacer(6, 26))
 
-    #Adding body
+    # Adding body
     p_font = 13
     text = """<font name=times-roman size=%s>{}</font>
         """.format(corps) % p_font
     Story.append(Paragraph(text, styles["Justify"]))
     Story.append(Spacer(6, 12))
 
-    #Saving pdf
+    # Saving pdf
     doc.build(Story)
     datas = [localPath, outfilename]
     return datas
