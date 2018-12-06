728dfae2241ee29ede6efb3019a074c528d1b4f0
mcomans <marc-henry.comans@student.uclouvain.be>
2018-12-06 15:43:25 +0100

    meilleur format de pdf

--- superform/superform/plugins/pdf.py
+++ superform/superform/plugins/pdf.py
@@ -8,6 +8,14 @@ from flask import url_for, redirect
 from reportlab.pdfgen import canvas
 import json
 from flask import current_app, request
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import Paragraph
+import os
 
 FIELDS_UNAVAILABLE = []
 
@@ -46,12 +54,52 @@ def create_pdf(titre, corps, image, size):
     elif(size=="A3"):
         realSize=(841.27, 1190.54)
 
-    pdf = canvas.Canvas(titre + ".pdf", realSize)
+    # pdf = canvas.Canvas(titre + ".pdf", realSize)
+    #
+    # write_logo(pdf,image, realSize)
+    # write_title(pdf, titre, realSize)
+    # write_body(pdf,corps,realSize)
+    # pdf.save()
+    titre.replace(" ","_")
+    outfilename = titre+".pdf"
+    PDF_DIR = request.url_root + "static/pdf/"
+    print(PDF_DIR)
+    outfilepath = os.path.join(PDF_DIR, outfilename)
+    doc = SimpleDocTemplate(outfilename, pagesize=letter,
+                            rightMargin=72, leftMargin=72,
+                            topMargin=72, bottomMargin=18)
 
-    write_logo(pdf,image, realSize)
-    write_title(pdf, titre, realSize)
-    write_body(pdf,corps,realSize)
-    pdf.save()
+    Story = []
+
+    #Adding logo
+    print("image path=",image)
+    print(os.curdir)
+    # /home/marc-henry/PycharmProjects/LINGI2255-2018-Superform-MS-06/superform/superform/plugins/logos/UCL.png
+
+    im = Image(image+".png")  # , 2 * inch, 2 * inch)
+    Story.append(im)
+    styles = getSampleStyleSheet()
+    styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY, leading=15))
+    Story.append(Spacer(1, 20))
+
+    #Adding title
+    p_font = 24
+    Story.append(Spacer(1, 30))
+    # ptext = '<font name=HELVETICA>'+title+'</font>' % p_font
+    rr = """<font name=times-roman size=%s>{}</font>
+        """.format(titre) % p_font
+    Story.append(Paragraph(rr, styles["Normal"]))
+    Story.append(Spacer(6, 26))
+
+    #Adding body
+    p_font = 13
+    text = """<font name=times-roman size=%s>{}</font>
+        """.format(corps) % p_font
+    Story.append(Paragraph(text, styles["Justify"]))
+    Story.append(Spacer(6, 12))
+
+    #Saving pdf
+    doc.build(Story)
 
 
 def write_logo(canvas, image, realSize):
